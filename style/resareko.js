e8d5350ab36cb6f4262b2f1169217a0e829e9fe9
elvira boman <elvira@deedster.com>
2019-03-26 20:58:50 +0100

    enkel m karta

--- style/resareko.js
+++
@@ -1,8 +0,0 @@
-function initialize() {
-    var mapOptions = {
-            center: new google.maps.LatLng(54, 15),
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            zoom: 3
-          };	 
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-};
